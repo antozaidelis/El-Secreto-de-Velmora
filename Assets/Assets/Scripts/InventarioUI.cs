4e17d3f4a7c2cfbe204f2db79e5a4303ee07f304
larakaida <lararamos@uade.edu.ar>
2026-06-12 17:21:43 -0300

    Add files via upload

---
+++ Assets/Assets/Scripts/InventarioUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class InventarioUI : MonoBehaviour
+{
+    [Header("Referencias")]
+    public GameObject panelMochila;
+    public List<Image> slots;
+
+    private bool mochilaAbierta = false;
+
+    public void ToggleMochila()
+    {
+        mochilaAbierta = !mochilaAbierta;
+        panelMochila.SetActive(mochilaAbierta);
+    }
+
+    public void ActualizarUI(List<Sprite> iconos)
+    {
+        for (int i = 0; i < slots.Count; i++)
+        {
+            if (i < iconos.Count)
+            {
+                slots[i].sprite = iconos[i];
+                slots[i].color = Color.white;
+            }
+            else
+            {
+                slots[i].sprite = null;
+                slots[i].color = new Color(1, 1, 1, 0.2f);
+            }
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        if (horizontal > 0)
+            transform.localScale = new Vector3(0.278f, 0.278f, 1);
+        else if (horizontal < 0)
+            transform.localScale = new Vector3(-0.278f, 0.278f, 1);
+
+        animator.SetBool("isWalking", horizontal != 0 && isGrounded);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+
+    {
+        if (col.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
+    }
+}
