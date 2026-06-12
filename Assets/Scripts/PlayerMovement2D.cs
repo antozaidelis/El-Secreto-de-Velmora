3b31978f7d6f0ef44f82f78ffb90ed7ef414d274
Antonella Zaidelis <antozaidelis@gmail.com>
2026-06-12 00:25:36 -0300

    Velmora_actualizado
    gatito saltando

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlayerMovement2D : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 8f;
     private Rigidbody2D rb;
     private Animator animator;
+    private bool isGrounded = true;
 
     void Start()
     {
@@ -12,10 +14,19 @@ public class PlayerMovement2D : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
     void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
 
         if (horizontal > 0)
@@ -23,6 +34,16 @@ public class PlayerMovement2D : MonoBehaviour
         else if (horizontal < 0)
             transform.localScale = new Vector3(-0.278f, 0.278f, 1);
 
-        animator.SetBool("isWalking", horizontal != 0);
+        animator.SetBool("isWalking", horizontal != 0 && isGrounded);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+
+    {
+        if (col.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
     }
 }
